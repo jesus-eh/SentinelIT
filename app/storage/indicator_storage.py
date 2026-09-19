5376ce8a998ece5954352a1f4817a6adf0549b14
jesus-eh <chechuescano981@gmail.com>
2026-09-19 19:39:38 +0200

    Ignore Python cache files

--- app/storage/indicator_storage.py
+++ app/storage/indicator_storage.py
@@ -22,22 +22,30 @@ def get_indicatorsID(db, id):
 # Uploading the indicators   
 def post_indicator(indicator, db):
     try:
-        indicator_db = Indicator(
-            type=indicator.type,
-            value=indicator.value,
-            source=indicator.source,
-            confidence=indicator.confidence,
-            threat_type=indicator.threat_type,
-            tags=indicator.tags
-        )
 
-        db.add(indicator_db)
-        db.commit()
-        db.refresh(indicator_db)
+        print("Entra en metodo")
+        busqueda = search_type_value(db, indicator.type, indicator.value)
 
-        return {"succes": "ok"}
+        if busqueda.get("succes") == "ok":
+            indicator_db = Indicator(
+                type=indicator.type,
+                value=indicator.value,
+                source=indicator.source,
+                confidence=indicator.confidence,
+                threat_type=indicator.threat_type,
+                tags=indicator.tags
+            )
+
+            db.add(indicator_db)
+            db.commit()
+            db.refresh(indicator_db)
+            return {"succes": "ok"}
+        else:
+            return {"succes":"error","msg": busqueda.get("msg")}
+
+
     except Exception as e:
-        return {"succes": "Error", "msg" : f" Indicators no subido {e}"}
+        return {"succes": "Error", "msg" : f" Indicators no subido -> {e}"}
 
 # Delete the indicator by id
 def del_indicator(id, db):
@@ -45,6 +53,7 @@ def del_indicator(id, db):
     try:
         consulta = select(Indicator).where(Indicator.id == id)
         resultado = db.execute(consulta)
+
         indicador = resultado.scalars().one_or_none()
         if indicador:
             db.delete(indicador)
@@ -55,3 +64,29 @@ def del_indicator(id, db):
 
     except Exception as e:
         return {"succes": "Error", "msg" : f" Indicators no eliminado: {e}"}
+
+
+# Method with search type and value to indicator
+def search_type_value(db, type, value):
+
+    consulta = select(Indicator).where(Indicator.type == type and Indicator.value == value)
+    resultado = db.execute(consulta)
+
+    data = resultado.scalars().all()
+    cont = 0
+    for datos in data:
+        if datos.type == type and datos.value == value:
+            cont = cont + 1
+
+    if cont > 0:
+        return {"succes":"error","msg": f"El tipo {type} y el value {value} ya estan registrado"}
+    else:
+        return {"succes":"ok"}
+
+
+# def loop_lista(data):
+
+#     for datos in data:
+        
+
+#     return
